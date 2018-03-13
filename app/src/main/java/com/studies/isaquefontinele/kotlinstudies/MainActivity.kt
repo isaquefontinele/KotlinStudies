977923d87fc5408ca7d722b656d54299d4023062
Isaque Fontinele <isaquefontinele@gmail.com>
2018-03-13 15:13:52 -0300

    First lines

--- app/src/main/java/com/studies/isaquefontinele/kotlinstudies/MainActivity.kt
+++ app/src/main/java/com/studies/isaquefontinele/kotlinstudies/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.studies.isaquefontinele.kotlinstudies
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var big: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun changeMario(view: View) {
+//        if (big) image_mario.setImageResource(R.drawable.marioSmall) else image_mario.setImageResource(R.drawable.marioBig)
+
+        big = if (big) {
+            image_mario.setImageResource(R.drawable.mario_small)
+            false
+        } else {
+            image_mario.setImageResource(R.drawable.mario_big)
+            true
+        }
+
     }
 }
